c95c0093d53ed3780e0bdaa5fd1f30e18c922a11
Michelle Ranly <meranly8@gmail.com>
2020-11-16 04:06:22 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -18,4 +18,7 @@ class InteractiveRecord
     values.join(", ")
   end
 
+  def save
+    DB[:conn].execute("INSERT INTO #{self.table_name_for_insert}(#{col_names_for_insert}) VALUES (?)", [values_for_insert]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
 end
