e831e344a592caafcc17afd84d672c4542f1caec
Michelle Ranly <meranly8@gmail.com>
2020-11-16 03:57:52 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,9 +2,7 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  def initialize(options={})
-    options.each do |property, value|
-      self.send("#{property}=", value)
-    end
-  end
+
+
+
 end
