0b636f236fe9dc0ba8dbaaaa4478f70220fb5eb3
Michelle Ranly <meranly8@gmail.com>
2020-11-16 05:12:25 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,23 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  attr_accessor :id, :name, :grade
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "PRAGMA table_info('#{self.table_name}')"
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+
+    table_info.each do |column|
+      column_names << column["name"]
+    end
+    column_names.compact
+  end
 
 end
