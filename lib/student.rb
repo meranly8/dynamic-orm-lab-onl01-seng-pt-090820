36d91cf50be03dfab9bbfc0dbe49e2723738033d
Michelle Ranly <meranly8@gmail.com>
2020-11-16 03:56:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,8 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  attr_accessor :id, :name
+
   def self.table_name
     self.to_s.downcase.pluralize
   end
