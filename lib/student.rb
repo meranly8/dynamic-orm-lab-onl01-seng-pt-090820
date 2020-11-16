36f750a2a8395b9a94e1352fb805cc57312cb9ba
Michelle Ranly <meranly8@gmail.com>
2020-11-16 03:57:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,12 @@ require 'interactive_record.rb'
 class Student < InteractiveRecord
   attr_accessor :id, :name, :grade
 
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+  
   def self.table_name
     self.to_s.downcase.pluralize
   end
